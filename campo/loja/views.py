8566ee1512be4d36d2e87fe5b515a252f968b7f3
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2024-07-04 17:38:29 +0000

    HttpResponse

--- campo/loja/views.py
+++ campo/loja/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def pesquisar(request):
-    return render(request, 'template/pesquisar.html')
+    return HttpResponse("Estou")
 
 def cadastrar(request):
     return render(request, 'template/cadastrar.html')
