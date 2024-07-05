162bf6dcf6969487e6eecc10251cbdf0b92fa1ea
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2024-07-05 18:23:20 +0000

    3

--- campo/loja/models.py
+++ campo/loja/models.py
@@ -6,4 +6,12 @@ class Produto(models.Model):
     name = models.CharField(max_length=150)
     preço = models.IntegerField()
 
+    def __str__(self):
+        return self.name
     
+class Fornecedor(models.Model):
+    name = models.CharField(max_length=150)
+    endereço = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
