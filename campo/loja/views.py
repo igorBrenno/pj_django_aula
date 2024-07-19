80e40264b25d975760603d1c8617496a9ad89c7e
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2024-07-19 18:28:47 +0000

    form pesquisa post

--- campo/loja/views.py
+++ campo/loja/views.py
@@ -4,6 +4,11 @@ from .models import Produto
 # Create your views here.
 
 def pesquisar(request):
+    if (request.method == "POST"):
+        pesquisa = request.POST.get("pesquisa")
+        print(pesquisa)
+        resp = Produto.objects.filter(name__contains=pesquisa)
+        return render(request, "loja/pesquisar.html", {'resultado':resp})
 
     resp = Produto.objects.all()
     # return HttpResponse(resp)
